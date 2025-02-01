aecdbe33d95c8041ea992746d6e6192c780aaf8f
JenniferA98 <jparreola@willamette.edu>
2025-01-31 19:21:57 -0800

    updated

--- projects/class/battle_dice/game.py
+++ projects/class/battle_dice/game.py
@@ -10,7 +10,25 @@ class Game:
         self.__player2 = player2
 
 
-    def attack(self):
+    def attack(self, attacker:Character, defender:Character):
         """Performs an attack where the attacker rolls a die to determine damage dealt"""
-        dice_roll = random.int(1,6)
+        dice_roll = random.randint(1,6)
+        scaled_attack = dice_roll * attacker.attack_power
+
+        defender.health -= scaled_attack
+        if defender.health <= 0:
+            defender.health = 0
+            print(f"{defender.name} has been defeated!")
+
+        return scaled_attack
+
+
+
+    def start_battle(self):
+        """Starts a turn based battle between two players"""
+        ##want it to be random as to who starts
+        self.__turn = random.randint(1,2)
         
+        while self.__player1.health > 0 and self.__player2.health > 0:
+
+    
